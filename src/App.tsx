3de140c7a91f43661cd850aabde1444cc804e0b1
Alexim-Condec <alexeysmirnov9453@gmail.com>
2024-08-26 12:23:09 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Layout/Header"
+import Join from "./components/Home/Join"
+import Win from "./components/Home/Win"
+import Round from "./components/Home/Round"
+import Winner from "./components/Home/Winner"
+import History from "./components/Home/History"
+import Gold from "./components/Home/Gold"
+import Question from "./components/Home/Question"
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+function App() {
+  return (
+    <TonConnectUIProvider manifestUrl="https://tonconnect-manifest.json">
+      <div className="App">
+        <header className="App-header">
+          
+          <Header />
+          <Join />
+          <Win />
+          <Round />
+          <Winner />
+          <History />
+          <Gold />
+          <Question />
+        </header>
+      </div>
+    </TonConnectUIProvider>
+  );
+}
+
+export default App;
